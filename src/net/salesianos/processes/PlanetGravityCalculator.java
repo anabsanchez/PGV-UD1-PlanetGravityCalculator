1c1754ce88a68f375c6bbbf778b6b3ee32dd3363
anabsanchez <anabsanchez.dev@gmail.com>
2024-10-13 20:43:26 +0100

    feat(PlanetGravityCalculator): calculator created

--- src/net/salesianos/processes/PlanetGravityCalculator.java
+++ src/net/salesianos/processes/PlanetGravityCalculator.java
@@ -2,4 +2,24 @@ package processes;
 
 public class PlanetGravityCalculator {
     
+    // Gravitational constant
+    private static final double G = 6.67430e-11; // m³/(kg·s²)
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+
+            System.out.println("No se proporcionaron los datos del planeta.");
+            return;
+        }
+
+        String[] data = args[0].split(",");
+        String name = data[0];
+        double mass = Double.parseDouble(data[1]);
+        double radius = Double.parseDouble(data[2]); 
+
+        double gravity = G * (mass / (radius * radius));
+
+        System.out.println(name + " -> " + gravity + " m/s²");
+    }
 }
