7374b9bc5bdced8e9b5a328d0a560022b6a33fa2
anabsanchez <anabsanchez.dev@gmail.com>
2024-10-13 20:01:10 +0100

    feat(FileHelper): Added file reading functionality

--- src/net/salesianos/utils/FileHelper.java
+++ src/net/salesianos/utils/FileHelper.java
@@ -1,4 +1,46 @@
 package utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 public class FileHelper {
 
+    public static ArrayList<String> getAllLines(String filePath) {
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath, StandardCharsets.UTF_8))) {
+
+            String currentLine = reader.readLine();
+
+            while (currentLine != null) {
+                lines.add(currentLine);
+                currentLine = reader.readLine();
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("No se pudo leer el archivo: " + filePath);
+        }
+        return lines;
+    }
+
+    public static double getGravity(File file) {
+
+        String result = "?";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
+
+            result = reader.readLine();
+
+        } catch (IOException e) {
+
+            System.out.println("No se pudo obtener el resultado del archivo.");
+        }
+        return Double.parseDouble(result);
+    }
 }
